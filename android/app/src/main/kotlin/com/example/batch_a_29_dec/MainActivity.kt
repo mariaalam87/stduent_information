fea2bdde5daf7ad1814ac06fd64ddbb598978552
jannatsuha242424@gmail.com <24101997Jannat>
2021-12-29 12:01:29 +0600

    1st commit

---
+++ android/app/src/main/kotlin/com/example/batch_a_29_dec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.batch_a_29_dec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:batch_a_29_dec/screen/log_in.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LogIn(),
+    );
+  }
+}
